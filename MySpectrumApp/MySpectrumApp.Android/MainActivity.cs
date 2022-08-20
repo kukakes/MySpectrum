ca4d02966a656ce6bd8b7998593e650a58dc0584
kuntalkishore <jkishok@jci.com>
2022-08-20 10:08:13 -0400

    Initial Setup

---
+++ MySpectrumApp/MySpectrumApp.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Prism;
+using Prism.Ioc;
+
+namespace MySpectrumApp.Android
+{
+    [Activity(Label = "MySpectrumApp", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App(new iAndroidInitializer()));
+        }
+    }
+
+    public class iAndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MySpectrumApp/MySpectrumApp/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace MySpectrumApp
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer platformInitializer)
+            :base(platformInitializer)
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnInitialized()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
