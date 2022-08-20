112089e4f2a36fc7cb488785019475c1386bb082
kuntalkishore <jkishok@jci.com>
2022-08-20 14:25:45 -0400

    Missed files

---
+++ MySpectrumApp/MySpectrumApp/Views/LoginView.xaml.cs
@@ -0,0 +1,13 @@
+using Xamarin.Forms.Xaml;
+
+namespace MySpectrumApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginView
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
