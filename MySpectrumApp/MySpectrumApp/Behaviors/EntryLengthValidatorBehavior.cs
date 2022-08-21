832c479a1c7093276deed4f94f98db7634ba6596
kuntalkishore <jkishok@jci.com>
2022-08-21 13:44:15 -0400

    Added Behaviors

---
+++ MySpectrumApp/MySpectrumApp/Behaviors/EntryLengthValidatorBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MySpectrumApp.Behaviors
+{
+    public class EntryLengthValidatorBehavior : Behavior<Entry>
+    {
+        int _minLength = 2;
+        Color _originalColor = Color.LightGray;
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnEntryTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnEntryTextChanged;
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var entry = (Entry)sender;
+            var text = entry.Text;
+
+            if (string.IsNullOrEmpty(text) ||
+                text.Length > _minLength)
+                entry.BackgroundColor = _originalColor;
+            else
+                entry.BackgroundColor = Color.Red;
+        }
+    }
+}
+
